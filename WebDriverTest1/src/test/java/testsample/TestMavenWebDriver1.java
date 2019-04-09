0c55209c38f6050f4739c41207e487c630fbf8ae
basireddys <reddys564@gmail.com>
2019-04-09 01:19:10 -0400

    TestNG Framework for Selenium Tests with Jenkins

---
+++ WebDriverTest1/src/test/java/testsample/TestMavenWebDriver1.java
@@ -0,0 +1,34 @@
+package testsample;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;
+
+public class TestMavenWebDriver1 {
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+		System.out.println("Test Passes, Voola");
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\shivakumar\\Eclipse\\geckodriver.exe");
+//	      FirefoxOptions options = new FirefoxOptions();
+//	  	  options.setLegacy(true);
+	    driver = new FirefoxDriver();  
+	    
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}	
+}
